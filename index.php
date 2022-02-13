e243e6b7dc17cd46bac03ea03307a4b2e193934e
ABiteOfSorrow <laceican@naver.com>
2022-02-13 21:32:56 +0100

    chore: Add index.php for heroku heploy

---
+++ index.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<html>
+  <head>
+    <title>ShareSkill</title>
+    <meta charset="utf-8">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">SHARESKILL</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Demande de Tuteur <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Liste de Classe</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Notre Objectif</a>
+      </li>
+    </ul>
+     <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#">S'inscrire <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Connexion</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item item1 active">
+        <div class="helper"></div><div class="intro">
+        <h2>APPRENDRE LA MUSIQUE</h2>
+        <h3>Il y a de grands musiciens autour.</h3>
+        <a href="#">Trouver une Classe</a>
+        </div>
+      </div>
+      <div class="carousel-item item2">
+        <div class="helper"></div><div class="intro">
+        <h2>APPRENDRE LE MAQUILLAGE</h2>
+        <h3>Il y a de super maquilleurs autour.</h3>
+        <a href="#">Trouver une Classe</a>
+        </div>
+      </div>
+        <div class="carousel-item item3">
+        <div class="helper"></div><div class="intro">
+        <h2>APPRENDRE à Cuisiner</h2>
+        <h3>Il y a de grands chefs autour.</h3>
+        <a href="#">Trouver une Classe</a>
+        </div>
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Précédent</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Suivant</span>
+    </a>
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+        <strong>Recrutement de Tuteurs!</strong> Partagez vos Talents.
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+        </button>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+        <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+          <a href="#" class="course">
+          <img src="images/course1.jpg" width="100%" class="course.img">
+            <div class="info">
+              <h4>Créez des illustrations uniques</h4>
+              <div class="lecturer">
+              <img src="images/user.png" width="20" height="20"> <span class="name">MoonJongMo</span>
+              </div>
+            </div>
+          </a>
+        </div>
+            <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+          <a href="#" class="course">
+          <img src="images/course2.jpg" width="100%" class="course.img">
+            <div class="info">
+              <h4>Créez mes propres accessoires</h4>
+              <div class="lecturer">
+              <img src="images/user.png" width="20" height="20"> <span class="name">HaJiEun</span>
+              </div>
+            </div>
+          </a>
+        </div>
+            <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+          <a href="#" class="course">
+          <img src="images/course3.jpg" width="100%" class="course.img">
+            <div class="info">
+              <h4>Mon histoire en chanson!</h4>
+              <div class="lecturer">
+              <img src="images/user.png" width="20" height="20"> <span class="name">KimShinEui</span>
+              </div>
+            </div>
+          </a>
+        </div>
+            <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+          <a href="#" class="course">
+          <img src="images/course4.jpg" width="100%" class="course.img">
+            <div class="info">
+              <h4>Programmation Web pour les non-majors </h4>
+              <div class="lecturer">
+              <img src="images/user.png" width="20" height="20"> <span class="name">SonDongUk</span>
+              </div>
+            </div>
+          </a>
+        </div>
+            <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+          <a href="#" class="course">
+          <img src="images/course5.jpg" width="100%" class="course.img">
+            <div class="info">
+              <h4>Retrouvez la confiance! cours de maquillage</h4>
+              <div class="lecturer">
+              <img src="images/user.png" width="20" height="20"> <span class="name">HwangMinYeon</span>
+              </div>
+            </div>
+          </a>
+        </div>
+            <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+          <a href="#" class="course">
+          <img src="images/course6.jpg" width="100%" class="course.img">
+            <div class="info">
+              <h4>Conquérir la danse des idoles</h4>
+              <div class="lecturer">
+              <img src="images/user.png" width="20" height="20"> <span class="name">ParkYounSeok</span>
+              </div>
+            </div>
+          </a>
+        </div>
+            <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+          <a href="#" class="course">
+          <img src="images/course7.jpg" width="100%" class="course.img">
+            <div class="info">
+              <h4>Apprendre la photographie avec un photographe</h4>
+              <div class="lecturer">
+              <img src="images/user.png" width="20" height="20"> <span class="name">YiYounSu</span>
+              </div>
+            </div>
+          </a>
+        </div>
+            <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+          <a href="#" class="course">
+          <img src="images/course8.jpg" width="100%" class="course.img">
+            <div class="info">
+              <h4>Cours de fleurs pour apprendre les bases</h4>
+              <div class="lecturer">
+              <img src="images/user.png" width="20" height="20"> <span class="name">SeoHyeRin</span>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>  
+
+    <div class="jumbotron jumbotron-fluid eventbanner">
+        <div class="container annonce1">
+          <h1 class="display-4 annonceTitle1">Annonce de la Conférence</h1>
+          <p class="lead annonceContent1">Date : 2022.02.28(Lundi)</p>
+          <p class="lead annonceContent1">Heure : 12:00 ~ 20:00</p>
+          <p class="lead annonceContent1">Lieu : 153 rue Gutenberg, Lille</p>
+          <p class="lead annonceContent1">Sujet : Discussion sur les moyens de dynamiser le don de talents</p>
+          <button type="button" class="btn btn-primary btn-lg conferenceBtn">Postulez pour participer</button>
+        </div>
+      </div>
+
+    <div class="jumbotron jumbotron-fluid associationMap">
+        <div class="container mapTitle1">
+          <h2 class="mapTitle1">Besoin d'aide?</h2>
+          <p class="lead mapContent1">Adress : 15 Pl. du Général de Gaulle, 59800 Lille</p>
+          <p class="lead mapContent1">Contact : 03 20 78 43 43</p>
+          <p class="lead mapContent1">Mail : shareskill@gmail.com</p>
+        </div>
+     </div>    
+      
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+</body>
+</html>
